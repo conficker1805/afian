229af0c3305dbdbcbce28fec32fbf1f315515c12
Eovy <conficker1805@gmail.com>
2015-11-29 22:15:50 +0700

    customize code for SEO

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -1,9 +1,8 @@
 module SeoHelper
 	def initialize_meta_tag
-		set_meta_tags :site 					=> "Afi - The API Layer for Cities",
-									:title 					=> "Afi - The API Layer for Cities",
-									:keywords 			=> %w[keyword1\ and\ Keyword2 Keyword3],					# Recommended keywords tag length: up to 255 characters, 20 words
-									:description 		=> "fleet management software, delivery tracking solution, delivery tracking system, route optimization, big data analytics", 		# Recommended up to 160 characters
+		set_meta_tags :title 					=> "Afi - The API Layer for Cities",
+									:keywords 			=> %w[big\ data\ analytics big\ data\ analytics\ tools visual\ analytics visual\ analytics\ tools visual\ analytics\ software predictive\ analytics fleet\ management\ software delivery\ tracking\ solution delivery\ tracking\ system route\ optimization ecommerce\ delivery\ software ecommerce\ delivery\ solutions logistics\ software logistics\ solutions supply\ chain\ software\ big\ data\ for\ transportation\ industry\ schedule\ and\ roster\ planning\ public\ transportation\ system\ public\ transportation\ management\ solution analytics\ in\ transportation\ industry public\ transportation\ management\ case\ studies ],					# Recommended keywords tag length: up to 255 characters, 20 words
+									:description 		=> "Afi builds Big Data analytics systems for cities and fleet management, delivery tracking software for logistics services", 		# Recommended up to 160 characters
 									:canonical 			=> "http://afi.io/",
 									:alternate 			=> { "en" => "http://afi.io/",
 																			 "vi" => "http://afi.io/" },
